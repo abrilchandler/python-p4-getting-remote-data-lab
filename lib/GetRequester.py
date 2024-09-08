d8c9ef54fa1672be3536e9bbd5576f3f44b206be
Chandler Abril <abrilchandler1@gmail.com>
2024-09-08 13:00:02 -0700

    Done

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,17 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        url = f'https://learn-co-curriculum.github.io/json-site-example/endpoints/people.jsonLinks to an external site.'
+        response = requests.get(self.url)
+        return response.content
+    
+
 
     def load_json(self):
-        pass
+        url = f'https://learn-co-curriculum.github.io/json-site-example/endpoints/people.jsonLinks to an external site.'
+        print(url)
+        response = requests.get(self.url)
+        return response.json()
+    
+results = GetRequester('https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json').get_response_body()
+print(results)
